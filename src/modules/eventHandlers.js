75e70734890cd9a97422a5eebfc25459fd24f306
Fezzopro <fezzopro@gmail.com>
2023-05-05 15:51:28 +0200

    Add eventHandler module to handle new taks event, and delent events

---
+++ src/modules/eventHandlers.js
@@ -0,0 +1,37 @@
+import storage from "./storage.js";
+import components from "./components.js";
+
+class EventHandlers {
+  constructor() {
+    this.description;
+    this.completed;
+    this.index;
+  }
+
+  newTaskEvent = () => {
+      const task = document.querySelector('.form-input').value;
+      this.description = task;
+      this.completed = false;
+      this.index = storage.readLocalStorage().length;
+
+      if (task.length > 0) {
+        storage.saveToLocalStorage({
+          description: this.description,
+          completed: this.completed,
+          index: this.index,
+        });
+        components.createTaskList(storage.readLocalStorage());
+      }
+  };
+
+  deleteTaskEvent = (bin) => {
+    storage.deleteFromLocalStorage(bin.firstChild.value);
+    components.createTaskList(storage.readLocalStorage());
+  };
+
+  updateTaskEvent = () => {};
+  completeTaskEvent = () => {};
+  clearAllTaskEvent = () => {};
+}
+
+export default new EventHandlers();
