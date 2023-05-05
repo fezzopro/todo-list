86eeb6402e73a7812f96e5f6589874e1470f5450
Fezzopro <fezzopro@gmail.com>
2023-05-05 02:04:24 +0200

    Correct Eslint Errors and add packages required for Eslint

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,14 +1,70 @@
 import _ from 'lodash';
-import '../css/app.css';
+import '../css/style.css';
 
-function component() {
-  const div = document.createElement('h1');
+const ul = document.querySelector('.todo-lists ul');
+const tasks = [
+  {
+    description: 'Organize your workspace',
+    completed: false,
+    index: 0
+  },
+  {
+    description: 'Catch up on household chores',
+    completed: false,
+    index: 1
+  },
+  {
+    description: 'Schedule time for self-care',
+    completed: true,
+    index: 2
+  },
+  {
+    description: 'Connect with loved ones',
+    completed: false,
+    index: 5
+  },
+  {
+    description: 'Complete a project at work',
+    completed: false,
+    index: 6
+  }
+];
 
-  // Lodash, now imported by this script
-  div.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  div.classList.add('hello');
+const createTaskList = (tasks) => {
+  if (tasks.length === 0 ) {
+    return null;
+  }
 
-  return div;
-}
+  tasks.forEach(task => {
+    ul.appendChild(createTaskListItem(task));
+  });
+};
 
-document.body.appendChild(component());
+const createTaskListItem = (singleTak) => {
+  const li = document.createElement('li');
+  li.className = 'list-item border-bottom';
+  const taskCkecksSpan = document.createElement('span');
+  taskCkecksSpan.className = 'task-ckecks';
+
+  const checkboxInput = document.createElement('input');
+  checkboxInput.type = 'checkbox';
+  checkboxInput.className = 'check-box';
+  (singleTak.completed === true) ? checkboxInput.setAttribute('checked','') : '';
+  checkboxInput.value = singleTak.index;
+
+  const descriptionSpan = document.createElement('span');
+  descriptionSpan.className = (singleTak.completed === false) ? 'description' : 'description completed';
+  descriptionSpan.textContent = singleTak.description;
+
+  const taskInfoSpan = document.createElement('span');
+  taskInfoSpan.className = 'task-info';
+
+  taskCkecksSpan.appendChild(checkboxInput);
+  taskCkecksSpan.appendChild(descriptionSpan);
+  li.appendChild(taskCkecksSpan);
+  li.appendChild(taskInfoSpan);
+
+  return li;
+};
+
+createTaskList(tasks);
