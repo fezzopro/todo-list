9b277c57c5d9b5a6aac98cb2fd04135acc58d643
AJ193 <119049613+AJ193@users.noreply.github.com>
2023-05-11 14:21:29 +0500

    Modify js File

--- test/storage.test.js
+++ test/storage.test.js
@@ -12,4 +12,20 @@ describe('Storage Module Test Suit', () => {
   test('isLocalStorage', () => {
     expect(!storage.isLocalStorage()).toBe(true);
   });
+  const mockBook = {
+    title: 'good man',
+    author: 'Bad Habits',
+    id: Math.ceil(Math.random() * 1000)
+  };
+  
+  test('isLocalStorage', () => {
+    storage.saveAsLocalSorage(mockBook);
+    expect(storage.isLocalStorage().length).toBeGreaterThan(0);
+  });
+
+
+  test('saveAsLocalSorage', () => {
+    storage.saveAsLocalSorage(mockBook);
+    expect(!storage.isLocalStorage()).toBe(false);
+  });
 });
